fefd93e66edcb29c8050cb771948b9f0743f3a0a
MinattiMel <melany.minatti02@gmail.com>
2024-11-26 16:51:26 +0100

    modifica component: griglia

--- components/Card.vue
+++ components/Card.vue
@@ -1,27 +1,31 @@
 <script setup>
 defineProps([
-    'title',' details', 'socialTitle','socialUrl', 'imageAlt', 'imageUrl'
+    'title','details', 'socialTitle','socialUrl', 'imageAlt', 'imageUrl'
 ])
 </script>
 
 <template>
-   <div>
+   <div class="grid grid-cols-2 px-8 py-8">
 
-       <h2>{{ title }}</h2>
-
-       <p>{{ details }}</p>
-
-       <img
+    <div class="cols-span-2">
+       <img class="rounded-full w-24"
            v-if="imageUrl"
            :alt="imageAlt"
            :src="imageUrl"
        />
+    </div>
+
+    <div class="cols-span-1">
+       <h2 class="text-2xl">{{ title }}</h2>
+
+       <p>{{ details }}</p>
 
        <a
            v-if="socialTitle && socialUrl"
            target="_blank"
            :href="socialUrl"
        >{{ socialTitle }}</a>
+    </div>
 
    </div>
 </template>
