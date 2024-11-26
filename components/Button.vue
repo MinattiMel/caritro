e1b588b12afa275e1bbf1e0ca9bef5dde72f4db1
MinattiMel <melany.minatti02@gmail.com>
2024-11-26 15:06:58 +0100

    creazione parametri button

--- components/Button.vue
+++ components/Button.vue
@@ -6,10 +6,11 @@ defineProps([
 
 <template>
 
-<a class="bg-red-500 font-thin text-center w-24 py-4 rounded"
+<a class="bg-secondary shadow-[0_0_4px_1px_rgba(255,255,255,1)] text-center w-96 px-4 py-2 text-3xl text-tertiary rounded-lg uppercase font-bold"
            v-if="text"
            :target="target"
            :href="link"
        >{{ text }}</a>
 
 </template>
+
