cf149ef591e634676317b892f0cca430d7ddb38d
MinattiMel <melany.minatti02@gmail.com>
2024-11-26 15:08:33 +0100

    creazione cards

--- pages/Home.vue
+++ pages/Home.vue
@@ -2,14 +2,13 @@
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 import Card from "../components/Card.vue";
-import Button from "../components/Button.vue";
 import Link from "../components/Link.vue";
 
 </script>
 
 <template>
 
-    <div class="xl:grid xl:gap-8 xl:grid-cols-3 p-8 lg:p-16 min-h-screen min-w-screen">
+    <div>
 
         <Header/>
 
@@ -21,23 +20,46 @@ import Link from "../components/Link.vue";
         />
 
         <Card
-            title="Rudy Zerbi"
-            details="Moderatore"
-            socialTitle="Titolo per il social"
+            title="Kid Yugi"
+            details="Artista musicale"
+            socialTitle="@kidyugiy3p"
+            socialUrl="https://www.instagram.com/kidyugiy3p/"
+        />
+
+        <Card
+            title="Il Rosso"
+            details="Content creator"
+            socialTitle="@ilrossopiubellodiinstagraam"
+            socialUrl="https://www.instagram.com/ilrossopiubellodiinstagraam/"
+
+        />
+
+        <Card
+            title="Livio Ricciardi"
+            details="Consulente sessuale"
+            socialTitle="@livioricciardi"
+            socialUrl="https://www.instagram.com/livioricciardi/"
+
         />
  
-        <Button 
-        text="prova"
+        <Card
+            title="Ciro Buccolieri"
+            details="Discografico e manager"
+            socialTitle="@cirobuccolieri"
+            socialUrl="https://www.instagram.com/cirobuccolieri/"
+
+        />
+
+        <Card
+            title="Serena Valorzi"
+            details="Psicoterapeuta"
+            socialTitle="@serena-valorzi"
+            socialUrl="https://it.linkedin.com/in/serena-valorzi-31235146"
+
         />
 
         <Link />
 
-        <div class="xl:justify-self-end flex flex-col gap-8 xl:col-start-2 xl:col-span-2 xl:row-span-2">
-            <div class="grid gap-2 xl:grid-cols-3 xl:pt-12">
-                <p class="xl:col-start-2 xl:col-span-2 text-3xl lg:text-5xl xl:text-7xl 2xl:text-8xl text-secondary [text-shadow:0px_0px_9px_#07fbfb]">20.01.2025</p>
-                <p class="xl:col-start-2 xl:col-span-2 text-xl 2xl:text-3xl">Auditorium S. Chiara - Trento | Ore 17 - 19</p>
-            </div>
-        </div>
 
         <Footer/>
 
