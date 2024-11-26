757117a8a17ab0e21742619d8cd3867accbc2433
Maicol Zenatti <maicol@pigmentolab.com>
2024-11-26 11:49:06 +0100

    button example

---
+++ components/ButtonExample.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(['text', 'target', 'link', 'variant'])
+</script>
+
+<template>
+   <a
+       v-if="link"
+       :class="
+        (variant === 'secondary' ? 'bg-amber-50 text-amber-600' : 'bg-amber-600 text-white')
+       "
+       :target="target"
+       :href="link"
+   >
+       {{text}}
+   </a>
+</template>
