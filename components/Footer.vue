caf66c78197bf70f2fcad23d362ce437a1916975
MinattiMel <melany.minatti02@gmail.com>
2024-11-26 15:07:47 +0100

    creazione component social

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,12 +12,6 @@
             <li><p class="text-base">Il sito verrà aggiornato, seguici:</p>
                 <a class="flex items-center" href="https://www.instagram.com/swipe.festival/" target="blank"><img class="w-8" alt="Instagram" src="/assets/logo-instagram.svg"> swipe.festival</a></li>
         </ul>
-        <h3 class="sr-only">Partner</h3>
-        <ul class="flex gap-12">
-            <li><a href="https://www.fondazionecaritro.it/" target="blank"><img class="h-16" alt="Fondazione Caritro" src="/assets/logo-caritro.svg"></a></li>
-            <li><img class="h-16" alt="Sideout" src="/assets/logo-sideout.svg"></li>
-        </ul>
-
          <Social />
 
     </footer>
--- components/Social.vue
+++ components/Social.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-<div>
 
-</div>
+    <h3 class="sr-only">Partner</h3>
+        <ul class="flex gap-8">
+            <li><a href="https://www.fondazionecaritro.it/" target="blank"><img class="h-8" alt="Fondazione Caritro" src="/assets/logo-caritro.svg"></a></li>
+            <li><img class="h-8" alt="Sideout" src="/assets/logo-sideout.svg"></li>
+        </ul>
+
 </template>
