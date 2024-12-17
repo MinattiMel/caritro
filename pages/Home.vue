a513b18fad5eae0910bd7325479132684673e51e
MinattiMel <melany.minatti02@gmail.com>
2024-12-17 10:00:21 +0100

    correzione icona biglietti

--- pages/Home.vue
+++ pages/Home.vue
@@ -139,7 +139,7 @@ import imgValorzi from "../assets/ospiti/serena-valorzi-def.jpg";
                     </li>
 
                     <li class="flex items-start space-x-4 lg:col-span-2">
-                        <a href="https://www.eventbrite.it/e/1096506202569?aff=oddtdtcreator"><img class="w-[40px] transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-lg hover:cursor-pointer" src="../assets/biglietto_icona.png" alt="Icona biglietto cartaceo"></a>
+                        <a class="" href="https://www.eventbrite.it/e/1096506202569?aff=oddtdtcreator"><img class="w-[40px] min-w-[40px] h-auto transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-lg hover:cursor-pointer" src="../assets/biglietto_icona.png" alt="Icona biglietto cartaceo"></a>
                         <div>
                             <p class="text-2xl lg:text-3xl mb-6">Biglietti dal 16.12.2024</p>
                             <p class="text-xl leading-7">
