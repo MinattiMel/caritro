45f9f01d9f8adbcfebe4f235abcce0cefbc11f7d
MinattiMel <melany.minatti02@gmail.com>
2025-03-11 10:07:24 +0100

    aggiunta colori

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,14 @@ export default {
                 tertiary: '#801b72',
                 quaternary: '#df0d78',
                 quinary: '#8c0876',
-                sextiary: '#7805ff'
+                sextiary: '#7805ff',
+                octree: '#31006B',
+                ninetree: '#F25500',
+                tentree: '#D13800',
+            },
+            screens: {
+                'mix-900': { 'raw': '(max-width: 900px)' },
+                'max-1000': { 'raw': '(max-width: 1000px)' }, /**non funziona */
             }
         }
     },
