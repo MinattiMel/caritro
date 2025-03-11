d612fb4c1705200ed009a2d83c54e2559c8e46f6
MinattiMel <melany.minatti02@gmail.com>
2025-03-11 10:06:51 +0100

    import masterclass su main js

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
 import ScorrendoTraLeGenerazioni from "./pages/01_ScorrendoTraLeGenerazioni.vue";
+import Masterclass from "./pages/masterclass.vue";
 
 // definisco le routes e le collego ad una componente
 const routes = [
     {path: '/scorrendo-tra-le-generazioni', component: ScorrendoTraLeGenerazioni},
     {path: '/', component: Home},
+    {path: '/masterclass', component: Masterclass},
+    //{path: '/', component: Home}
 ]
 
 const router = createRouter({
