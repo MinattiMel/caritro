d4ad4f857b6347229eab76ee00bd36d1ccf7374a
MinattiMel <melany.minatti02@gmail.com>
2025-03-11 10:01:33 +0100

    Create Button-giallo.vue
    creazione button giallo

---
+++ components/Button-giallo.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps([
+    'link','text','target'
+])
+</script>
+
+<template>
+
+    <a
+        class="bg-[#FFAE00] text-center w-96 px-4 py-2 text-3xl text-[#D13800] rounded-lg uppercase font-bold"
+        v-if="text"
+        :target="target"
+        :href="link"
+   >
+        {{ text }}
+    </a>
+
+</template>
+
