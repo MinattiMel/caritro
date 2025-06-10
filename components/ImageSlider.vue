362875a2a29011e40fdf316bb8fed1f814e4d299
Maicol Zenatti <maicol@pigmentolab.com>
2025-06-10 11:11:56 +0200

    fix slider

--- components/ImageSlider.vue
+++ components/ImageSlider.vue
@@ -33,22 +33,22 @@ function closeModal() {
 
 <template>
   <div class="w-full max-w-5xl mx-auto">
-    <Swiper :modules="[Autoplay, Pagination, Keyboard]" :slides-per-view="1" :loop="true"
-      :autoplay="{ delay: 4000, disableOnInteraction: false }" :keyboard="{ enabled: true }" pagination
+    <Swiper :modules="[Autoplay, Pagination, Keyboard]" :slides-per-view="1" pagination
       class="w-full h-[420px] lg:h-[700px]">
       <SwiperSlide v-for="(src, idx) in imageSliderURL" :key="idx" class="flex items-center justify-center">
         <div class="h-full flex items-center justify-center">
 
           <!-- Link per quell'immagine -->
-          <a v-if="imageSliderLink[idx]" :href="imageSliderLink[idx]" target="_blank" rel="noopener noreferrer">
-            <img :src="src" :alt="imageSliderALT[idx] || `Immagine ${idx + 1}`"
-              class="object-contain max-h-[350px] lg:max-h-[600px] max-w-[90%] rounded-2xl cursor-pointer transition-transform duration-200 hover:scale-105" />
+          <a v-if="imageSliderLink[idx]" :href="imageSliderLink[idx]" target="_blank" rel="noopener noreferrer"
+            class="object-contain max-h-[350px] lg:max-h-[600px] max-w-[90%] cursor-pointer transition-transform duration-200 hover:scale-105 block">
+            <img :src="src" :alt="imageSliderALT[idx] || `Immagine ${idx + 1}`" class="rounded-2xl" />
           </a>
 
           <!-- Apri la modale al click -->
           <img v-else :src="src" :alt="imageSliderALT[idx] || `Immagine ${idx + 1}`"
             class="object-contain max-h-[400px] lg:max-h-[600px] max-w-[90%] rounded-2xl cursor-pointer transition-transform duration-200 hover:scale-105"
             @click="openImage(src)" />
+
         </div>
       </SwiperSlide>
     </Swiper>
